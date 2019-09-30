9bfd6ca646e3e3e327da625535d7c646d8498ffd
Dan <dsinger.sax@gmail.com>
2019-09-29 22:34:00 -0400

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+
+http.createServer((request, response) => {
+
+}).listen(PORT);
