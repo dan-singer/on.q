a3e31e1a425a96b4bafcecbeb9ab0e30b4b7e703
Dan Singer (RIT Student) <djs5435@ad.rit.edu>
2019-10-02 14:48:32 -0400

    Begin landing page layout and styling

---
+++ client/landing.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    let makeEventButton = document.querySelector("#make-event");
+    let eventForm = document.querySelector("#event-form");
+    makeEventButton.onclick = () => {
+        // TODO form toggling
+    };
+};
--- src/static-file-handler.js
+++ src/static-file-handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const serveStaticFile = (request, response, filePath, contentType) => {
-  const file = path.resolve(`${__dirname}/../client/`, filePath.substring(1)); // Trim off the leading '/'
+  const file = path.resolve(`${__dirname}/../out/`, filePath.substring(1)); // Trim off the leading '/'
   if (!fs.existsSync(file)) {
     serveStaticFile(request, response, '/not-found.html', 'text/html');
     return;
